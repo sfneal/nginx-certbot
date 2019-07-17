b47df973bcee071162af6f65c619b8475a8363f8
mrstephenneal <stephen.neal14@gmail.com>
2019-07-17 11:29:41 -0400

    Initial commit

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker build -t stephenneal/nginx-certbot:1.17-alpine-v1 ./1.17-alpine-v1/
---
+++ push.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+sh ./build.sh
+
+docker push stephenneal/nginx-certbot:1.17-alpine-v1
