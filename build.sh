0d038959f79de5f4550c6aa0e8b4355acdf67924
mrstephenneal <stephen.neal14@gmail.com>
2019-11-05 13:40:42 -0500

    ADD absolute directory resolving to build and push shell scripts

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-docker build -t stephenneal/nginx-certbot:1.17-alpine-v1 ./1.17-alpine-v1/
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+docker build -t stephenneal/nginx-certbot:1.17-alpine-v1 "${DIR}"/1.17-alpine-v1/
--- push.sh
+++ push.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-sh ./build.sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+sh "${DIR}"/build.sh
 
 docker push stephenneal/nginx-certbot:1.17-alpine-v1
