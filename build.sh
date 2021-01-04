259ea3bcb9988dbf9f110590aab80ae2d0280a9d
Stephen Neal <stephen.neal14@gmail.com>
2021-01-04 12:15:04 -0500

    OPTIMIZE Travis CI config by adding env matrix & param passing to build.sh script
    FIX build.sh to accept arguments.

--- build.sh
+++ build.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
 
+# Base directory containing source code
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-docker build -t stephenneal/nginx-certbot:1.17-alpine-v1 "${DIR}"/1.17-alpine-v1/
-docker build -t stephenneal/nginx-certbot:1.18-alpine-v1 "${DIR}"/1.18-alpine-v1/
-docker build -t stephenneal/nginx-certbot:1.19-alpine-v1 "${DIR}"/1.19-alpine-v1/
+# Optional TAG argument (if set, only the specified image will be built)
+TAG=${1:-null}
+
+# Check if the TAG variable is set
+if [ -z "$TAG" ]
+
+  # Only build one image
+  then
+    docker build -t stephenneal/nginx-certbot:"${TAG}" "${DIR}"/"${TAG}"/
+
+  # Build all images
+  else
+    docker build -t stephenneal/nginx-certbot:1.17-alpine-v1 "${DIR}"/1.17-alpine-v1/
+    docker build -t stephenneal/nginx-certbot:1.18-alpine-v1 "${DIR}"/1.18-alpine-v1/
+    docker build -t stephenneal/nginx-certbot:1.19-alpine-v1 "${DIR}"/1.19-alpine-v1/
+fi
