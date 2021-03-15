4f4d63b433d65c5296e4393b5623df48748d6fe3
Stephen Neal <stephen.neal14@gmail.com>
2021-03-15 12:51:42 -0400

    ADD 'deploy' section to Travis CI config for pushing Docker images to Docker Hub

--- push.sh
+++ push.sh
@@ -2,8 +2,22 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-sh "${DIR}"/build.sh
+# Optional TAG argument (if set, only the specified image will be built)
+TAG=${1:-null}
 
-docker push stephenneal/nginx-certbot:1.17-alpine-v1
-docker push stephenneal/nginx-certbot:1.18-alpine-v1
-docker push stephenneal/nginx-certbot:1.19-alpine-v1
+# Check if the TAG variable is set
+if [ "$TAG" != null ]
+
+  # Only build & push one image
+  then
+    sh "${DIR}"/build.sh "${TAG}"
+    docker push stephenneal/nginx-certbot:"${TAG}"
+
+  # Build & push all images
+  else
+    sh "${DIR}"/build.sh
+
+    docker push stephenneal/nginx-certbot:1.17-alpine-v1
+    docker push stephenneal/nginx-certbot:1.18-alpine-v1
+    docker push stephenneal/nginx-certbot:1.19-alpine-v1
+fi
